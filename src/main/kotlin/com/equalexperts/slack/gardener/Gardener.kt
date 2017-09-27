563ec004f967acc24f5da8f8b89e9326d507df4a
Sean Reilly <sreilly@equalexperts.com>
2017-09-27 13:52:39 +0100

    Move back to parallel streams

--- src/main/kotlin/com/equalexperts/slack/gardener/Gardener.kt
+++ src/main/kotlin/com/equalexperts/slack/gardener/Gardener.kt
@@ -27,7 +27,7 @@ class Gardener (private val slackApi: SlackApi, private val slackBotApi: SlackBo
             val channels = slackApi.listChannels().channels
             println("${channels.size} channels found")
 
-            val data = channels.stream()
+            val data = channels.parallelStream()
                 .filter { this.isEligibleForGardening(it) }
                 .map { Tuple(it, this.determineChannelState(it)) }
                 .peek { (it, state) ->
