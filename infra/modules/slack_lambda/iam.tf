7ee17dd5745e8d4b6a8e8ae517c615f79e5bc001
Adam Hansrod <adamhansrod@gmail.com>
2019-01-17 16:42:32 +0000

    Use SSM for slack api secret management
    Remove old konfig/environment variables way of using secrets

--- infra/modules/slack_lambda/iam.tf
+++ infra/modules/slack_lambda/iam.tf
@@ -18,8 +18,14 @@ POLICY
 }
 
 # Attach role to Managed Policy
-resource "aws_iam_policy_attachment" "ee_slack_gardener_iam_policy_attach" {
+resource "aws_iam_policy_attachment" "ee_slack_gardener_lambda_basic_policy_attach" {
   name = "EESlackGardenerLambdaExecPolicy"
   roles = ["${aws_iam_role.ee_slack_gardener_iam_role.id}"]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_policy_attachment" "ee_slack_gardener_ssm_policy_attach" {
+  name = "EESlackGardenerLambdaExecPolicy"
+  roles = ["${aws_iam_role.ee_slack_gardener_iam_role.id}"]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
 }
