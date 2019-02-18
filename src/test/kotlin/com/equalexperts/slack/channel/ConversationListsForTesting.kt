ba890eddfb02dc9c648fe1eaabb50a765c1faca6
Adam Hansrod <ahansrod@equalexperts.com>
2019-02-18 16:13:34 +0000

    Move test objects to test packages

--- src/test/kotlin/com/equalexperts/slack/channel/ConversationListsForTesting.kt
+++ src/test/kotlin/com/equalexperts/slack/channel/ConversationListsForTesting.kt
@@ -7,7 +7,9 @@ import com.equalexperts.slack.api.rest.model.ResponseMetadata
 object ConversationListsForTesting {
 
     fun withEmptyCursorToken(conversation: Conversation) = ConversationList(listOf(conversation), ResponseMetadata(""))
+    fun withEmptyCursorToken(conversations: List<Conversation>) = ConversationList(conversations, ResponseMetadata(""))
     fun withCursorToken(conversation: Conversation, cursor_token: String) = ConversationList(listOf(conversation), ResponseMetadata(cursor_token))
+    fun withCursorToken(conversations: List<Conversation>, cursor_token: String) = ConversationList(conversations, ResponseMetadata(cursor_token))
 
 
 }
