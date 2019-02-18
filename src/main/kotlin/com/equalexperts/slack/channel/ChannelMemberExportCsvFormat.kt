db34117e9481b0af101ad59a0c47a687c775dec6
Adam Hansrod <ahansrod@equalexperts.com>
2019-02-18 15:17:45 +0000

    Revert "Remove channel member export class"
    This reverts commit 9e00587a04f0b484ec438265ace3ac37e091628d.

---
+++ src/main/kotlin/com/equalexperts/slack/channel/ChannelMemberExportCsvFormat.kt
@@ -0,0 +1,32 @@
+package com.equalexperts.slack.channel
+
+import com.equalexperts.slack.api.conversations.ConversationsSlackApi
+import com.equalexperts.slack.api.users.UsersSlackApi
+import com.equalexperts.slack.api.users.model.UserId
+import org.slf4j.LoggerFactory
+import kotlin.system.measureNanoTime
+
+class ChannelMemberExportCsvFormat(private val conversationApi: ConversationsSlackApi, private val usersSlackApi: UsersSlackApi, private val channelname: String) {
+    private val logger = LoggerFactory.getLogger(this::class.java.name)
+
+    fun process() {
+        val nanoTime = measureNanoTime {
+            val eeAlumniChannel = ConversationsSlackApi.listAll(conversationApi).first { it.name == channelname }
+            val members = conversationApi.members(eeAlumniChannel)
+
+            val users = members.members
+                    .map { it -> UserId(it) }
+                    .map { it -> usersSlackApi.getUserInfo(it) }
+                    .map { it -> "${it.user.name}, ${it.user.is_restricted}, ${it.user.is_ultra_restricted} \n" }
+
+
+            println("name, single, multi\n")
+            for (user in users) {
+                println(user)
+            }
+        }
+
+        logger.info("done in ${nanoTime / 1_000_000} ms")
+    }
+
+}
